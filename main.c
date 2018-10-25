8c4e04c5d130180f1b8df5779831316c410aed06
ludovicm67 <ludovicmuller1@gmail.com>
2018-10-25 23:13:53 +0200

    parse interface option

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include <pcap/pcap.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
+// will handle each packet
 void packet_handler(unsigned char *args, const struct pcap_pkthdr *header,
                     const unsigned char *packet) {
   int i;
@@ -14,12 +16,40 @@ void packet_handler(unsigned char *args, const struct pcap_pkthdr *header,
   printf("\n");
 }
 
-int main(int argc, char const *argv[]) {
-  char *interface = "lo";
+// show the user how to run the program correctly
+void usage(char * program_name) {
+  fprintf(stderr, "Usage: %s ", program_name);
+  fprintf(stderr, "[-i interface]\n");
+
+  exit(EXIT_FAILURE);
+}
+
+int main(int argc, char *argv[]) {
+  int nberrors, c;
+  char *interface;
   char errbuf[PCAP_ERRBUF_SIZE];
   bpf_u_int32 mask;
   bpf_u_int32 net;
   pcap_t *session;
+
+  nberrors = 0;
+
+  // parse options
+  while ((c = getopt(argc, argv, "+i:")) != EOF) {
+    switch (c) {
+      case 'i':
+        interface = optarg;
+        break;
+      case '?':
+        nberrors++;
+        break;
+    }
+  }
+
+  // if something went wrong
+  if (nberrors || !interface) {
+    usage(argv[0]);
+  }
 
   // get device informations
   if (pcap_lookupnet(interface, &net, &mask, errbuf) == -1) {
