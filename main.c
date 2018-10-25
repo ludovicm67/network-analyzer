020e2e2451169c8e54780cce3aaae7bdd16509af
ludovicm67 <ludovicmuller1@gmail.com>
2018-10-25 23:24:19 +0200

    check device existance

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 // will handle each packet
 void packet_handler(unsigned char *args, const struct pcap_pkthdr *header,
@@ -19,14 +20,15 @@ void packet_handler(unsigned char *args, const struct pcap_pkthdr *header,
 // show the user how to run the program correctly
 void usage(char * program_name) {
   fprintf(stderr, "Usage: %s ", program_name);
-  fprintf(stderr, "[-i interface]\n");
+  fprintf(stderr, "[-i device]\n");
 
   exit(EXIT_FAILURE);
 }
 
 int main(int argc, char *argv[]) {
-  int nberrors, c;
-  char *interface;
+  int nberrors, c, found;
+  char *device;
+  pcap_if_t *devices, *dev_tmp;
   char errbuf[PCAP_ERRBUF_SIZE];
   bpf_u_int32 mask;
   bpf_u_int32 net;
@@ -38,7 +40,7 @@ int main(int argc, char *argv[]) {
   while ((c = getopt(argc, argv, "+i:")) != EOF) {
     switch (c) {
       case 'i':
-        interface = optarg;
+        device = optarg;
         break;
       case '?':
         nberrors++;
@@ -47,20 +49,42 @@ int main(int argc, char *argv[]) {
   }
 
   // if something went wrong
-  if (nberrors || !interface) {
+  if (nberrors || !device) {
     usage(argv[0]);
   }
 
+  // fetch all available devices
+  if (pcap_findalldevs(&devices, errbuf) == -1) {
+    perror("pcap_findalldevs failed");
+    exit(EXIT_FAILURE);
+  }
+
+  found = 0;
+  for (dev_tmp = devices; dev_tmp; dev_tmp = dev_tmp->next) {
+    if (!strcmp(dev_tmp->name, device)) {
+      found++;
+      break;
+    }
+  }
+
+  // free devices list
+  pcap_freealldevs(devices);
+
+  // check if device exists
+  if (!found) {
+    fprintf(stderr, "Device '%s' not found!\n", device);
+  }
+
   // get device informations
-  if (pcap_lookupnet(interface, &net, &mask, errbuf) == -1) {
-    fprintf(stderr, "Can't get netmask for device %s\n", interface);
+  if (pcap_lookupnet(device, &net, &mask, errbuf) == -1) {
+    fprintf(stderr, "Can't get netmask for device %s\n", device);
     net = 0;
     mask = 0;
   }
 
   // open device for reading
-  if ((session = pcap_open_live(interface, BUFSIZ, 0, -1, errbuf)) == NULL) {
-    fprintf(stderr, "Couldn't open device %s: %s\n", interface, errbuf);
+  if ((session = pcap_open_live(device, BUFSIZ, 0, -1, errbuf)) == NULL) {
+    fprintf(stderr, "Couldn't open device %s: %s\n", device, errbuf);
     exit(EXIT_FAILURE);
   }
 
