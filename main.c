7f41f4445a512eeba05e07e26d95bc29c58d9eb6
ludovicm67 <ludovicmuller1@gmail.com>
2018-10-25 23:01:52 +0200

    display some informations about catched packets

--- main.c
+++ main.c
@@ -2,9 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void got_packet(unsigned char *args, const struct pcap_pkthdr *header,
-                const unsigned char *packet) {
-  printf("got packet!\n");
+void packet_handler(unsigned char *args, const struct pcap_pkthdr *header,
+                    const unsigned char *packet) {
+  int i;
+  printf("got packet with length=%d\n", header->len);
+
+  // for the moment print the content of the packet (output can be strange)
+  for (i = 0; i < header->len; i++) {
+    printf("%c", packet[i]);
+  }
+  printf("\n");
 }
 
 int main(int argc, char const *argv[]) {
@@ -12,22 +19,26 @@ int main(int argc, char const *argv[]) {
   char errbuf[PCAP_ERRBUF_SIZE];
   bpf_u_int32 mask;
   bpf_u_int32 net;
-  pcap_t *handle;
+  pcap_t *session;
 
+  // get device informations
   if (pcap_lookupnet(interface, &net, &mask, errbuf) == -1) {
     fprintf(stderr, "Can't get netmask for device %s\n", interface);
     net = 0;
     mask = 0;
   }
 
-  if ((handle = pcap_open_live(interface, BUFSIZ, 0, -1, errbuf)) == NULL) {
+  // open device for reading
+  if ((session = pcap_open_live(interface, BUFSIZ, 0, -1, errbuf)) == NULL) {
     fprintf(stderr, "Couldn't open device %s: %s\n", interface, errbuf);
     exit(EXIT_FAILURE);
   }
 
-  pcap_loop(handle, -1, got_packet, NULL);
+  // handle each packet
+  pcap_loop(session, -1, packet_handler, NULL);
 
-  pcap_close(handle);
+  // close pcap session
+  pcap_close(session);
 
   return EXIT_SUCCESS;
 }
